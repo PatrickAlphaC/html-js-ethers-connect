61e78bc734a6993acac6716de819fdf2c07ba9c6
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-01-31 13:08:21 -0500

    updated for executing a transaction

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# front-end-metamask-button
+# html-js-ethers-connect
 
 A small html file to show how to connect with metamask from the front-end. This is a minimalistic example what you can find in the [metamask docs](https://docs.metamask.io/guide/create-dapp.html#basic-action-part-1).
 
@@ -7,24 +7,34 @@ A small html file to show how to connect with metamask from the front-end. This 
 - [git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
   - You'll know you've installed it right if you can run:
     - `git --version`
-- [http-server](https://www.npmjs.com/package/http-server)
-  - You'll know you've installed it right if you can run:
-    - `http-server --version`
-    - You may need to install [Nodejs & npm](https://nodejs.org/en/download/)
+- [Nodejs & npm](https://nodejs.org/en/)
+  - You'll know you've installed nodejs right if you can run:
+    - `node --version` And get an ouput like: `vx.x.x`
+  - You'll know you've installed npm right if you can run:
+    - `npm --version` And get an ouput like `x.x.x`
+    - You might need to install it with npm
+- [Yarn](https://classic.yarnpkg.com/lang/en/docs/install/) instead of `npm`
+  - You'll know you've installed yarn right if you can run:
+    - `yarn --version` And get an output like: `x.x.x`
+    - You might need to install it with npm
+- [Metamask](https://metamask.io/)
+  - This is a browser extension that lets you interact with the blockchain.
 
 # Quickstart
 
-1. Clone the repo
+1. Clone the repo, install dependencies, and create the build.
 
 ```
 git clone https://github.com/PatrickAlphaC/front-end-metamask-button
 cd front-end-metamask-button
+yarn
+yarn build
 ```
 
 2. Serve the file
 
 ```
-http-server
+yarn http-server
 ```
 
 And you'll see an output like:
@@ -39,3 +49,32 @@ Hit CTRL-C to stop the server
 Copy paste the first link into your browser, and you should see a small button that says "connect".
 
 ![Connect](connect.png)
+
+Hit it, and you should see metamask pop up.
+
+# Execute a transaction
+
+If you want to execute a transaction follow this:
+
+Make sure you have the following installed:
+
+1. You'll need to open up a second terminal and run:
+
+```
+git clone https://github.com/PatrickAlphaC/hardhat-simple-storage
+cd hardhat-simple-storage
+yarn hardhat node
+```
+
+This will deploy a sample contract and start a local hardhat blockchain.
+
+2. Connect your [metamask](https://metamask.io/) to your local hardhat blockchain.
+
+> **PLEASE USE A METAMASK ACCOUNT THAT ISNT ASSOCIATED WITH ANY REAL MONEY.**
+> I usually use a few different browser profiles to separate my metamasks easily.
+
+In the output of the above command, take one of the private key accounts and [import it into your metamask.](https://metamask.zendesk.com/hc/en-us/articles/360015489331-How-to-import-an-Account)
+
+Additionally, add your localhost with chainid 31337 to your metamask.
+
+3. Hit the `Execute` button after connecting
---
+++ index.js
@@ -0,0 +1,124 @@
+const { ethers } = require("ethers");
+
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      await ethereum.request({ method: "eth_requestAccounts" });
+    } catch (error) {
+      console.log(error);
+    }
+    document.getElementById("metamask").innerHTML = "Connected";
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    console.log(accounts);
+  } else {
+    document.getElementById("metamask").innerHTML = "Please install MetaMask";
+  }
+}
+
+async function execute() {
+  if (typeof window.ethereum !== "undefined") {
+    contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const abi = [
+      {
+        inputs: [
+          {
+            internalType: "string",
+            name: "_name",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "_favoriteNumber",
+            type: "uint256",
+          },
+        ],
+        name: "addPerson",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "string",
+            name: "",
+            type: "string",
+          },
+        ],
+        name: "nameToFavoriteNumber",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        name: "people",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "favoriteNumber",
+            type: "uint256",
+          },
+          {
+            internalType: "string",
+            name: "name",
+            type: "string",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "retrieve",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "uint256",
+            name: "_favoriteNumber",
+            type: "uint256",
+          },
+        ],
+        name: "store",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+    ];
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      await contract.store(42);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    document.getElementById("metamask").innerHTML = "Please install MetaMask";
+  }
+}
+
+module.exports = {
+  connect,
+  execute,
+};
